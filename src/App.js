4db129b97a6d3f786f3bf83656713aca9058e768
Jeff mwai <jefylionel@gmail.com>
2023-11-15 13:04:06 +0300

    edited App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import './App.css'; // You can create this file for styling
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the local server
+    fetch('http://localhost:8001/transactions')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Bank Transactions App</h1>
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      <TransactionTable transactions={filteredTransactions} />
     </div>
   );
-}
+};
 
 export default App;
